2ee336c23e07fc867f44ecc44e5caa6d72716064
64070040Thanakorn <88426940+64070040Thanakorn@users.noreply.github.com>
2023-09-28 00:05:03 +0700

    [add] edit post, report post, [update] add comment to each line in code (post)

--- navigation/mainNavigator.js
+++ navigation/mainNavigator.js
@@ -5,16 +5,19 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
 import BottomNavigator from "./bottomNavigator";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "@ant-design/react-native";
 
 const MainStackNavigator = createNativeStackNavigator();
 
 export default function MainNavigator() {
     return (
-        <NavigationContainer>
-            <MainStackNavigator.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
-                <MainStackNavigator.Screen name="Login" component={LoginScreen}/>
-                <MainStackNavigator.Screen name="App" component={BottomNavigator} />
-            </MainStackNavigator.Navigator>
-        </NavigationContainer>
+        <Provider>
+            <NavigationContainer>
+                <MainStackNavigator.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+                    <MainStackNavigator.Screen name="Login" component={LoginScreen}/>
+                    <MainStackNavigator.Screen name="App" component={BottomNavigator} />
+                </MainStackNavigator.Navigator>
+            </NavigationContainer>
+        </Provider>
     )
 }
