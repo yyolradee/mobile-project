749dc0253e702e86b55b8b9dff6f1bc5cc2784ac
64070040Thanakorn <88426940+64070040Thanakorn@users.noreply.github.com>
2023-10-08 06:41:44 +0700

    [update, add] update filter 90%, add post global state, update code to use global state, remove unnecessary code

--- store/index.js
+++ store/index.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from "redux"
 import drawerReducer from "./reducers/drawerReducer"
+import postReducer from "./reducers/postReducer"
 
 const rootReducer = combineReducers({
-  drawer: drawerReducer
+  drawer: drawerReducer,
+  post: postReducer
 })
 
 const store = createStore(rootReducer);
